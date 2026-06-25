6e7de609c335f994aa919ca129a5d8839690d35a
saqibbedar <chandio.saqib.m@gmail.com>
2026-06-25 21:56:51 +0500

    feat(py26): add output formatter programs & cheat sheet

---
+++ python_2026/0x_output_formators/04_percentage.py
@@ -0,0 +1,3 @@
+rate = 0.853
+
+print(f"{rate:.1%}")            # output: 85.3%
---
+++ python_2026/0x_output_formators/07_debug_fmt.py
@@ -0,0 +1,6 @@
+# Debugging formatting 
+
+name = "Bedar"
+age = 21
+
+print(f"{name=} | {age=}")          # Output: name='Bedar' | age=21
